4c014d39545418240b431a2607d8189927548b0e
suoravi <ville.m.suoranta@gmail.com>
2025-06-07 19:36:25 +0300

    opens full screen

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 #include "components/window.h"
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#define INTERNAL_WIDTH 640
+#define INTERNAL_HEIGHT 480
 
 int main(void) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -13,12 +13,18 @@ int main(void) {
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("Draggable Window Demo",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SCREEN_WIDTH, SCREEN_HEIGHT,
-                                          SDL_WINDOW_SHOWN);
+    SDL_DisplayMode display_mode;
+    if (SDL_GetCurrentDisplayMode(0, &display_mode) != 0) {
+        fprintf(stderr, "SDL_GetCurrentDisplayMode failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
 
+    SDL_Window *window = SDL_CreateWindow("Draggable Window Fullscreen",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          display_mode.w, display_mode.h,
+                                          SDL_WINDOW_FULLSCREEN);
     if (!window) {
         fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
         SDL_Quit();
@@ -32,6 +38,10 @@ int main(void) {
         SDL_Quit();
         return 1;
     }
+
+    // Keep internal 640x480 layout regardless of screen resolution
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"); // Linear filtering
+    SDL_RenderSetLogicalSize(renderer, INTERNAL_WIDTH, INTERNAL_HEIGHT);
 
     GUIWindow gui;
     gui_window_init(&gui, 100, 100, 300, 200);
